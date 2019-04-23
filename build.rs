c020f39fbbfc20bd6859b7359da35d52bbdb1510
Florian Kothmeier <floriankothmeier@web.de>
2019-04-23 22:49:13 +0200

    Automatic discovery of include paths

--- build.rs
+++ build.rs
@@ -1,9 +1,15 @@
 extern crate cc;
+extern crate pkg_config;
 
 fn main() {
-    cc::Build::new()
-        .file("src/signal.cpp")
-        .include("/usr/include/x86_64-linux-gnu/qt5/QtCore")
-        .include("/usr/include/x86_64-linux-gnu/qt5")
-        .compile("signal");
+    let lib = pkg_config::probe_library("Qt5Core").unwrap();
+
+    let mut builder = cc::Build::new();
+    builder.file("src/signal.cpp");
+
+    for path in lib.include_paths {
+        builder.include(path);
+    }
+    
+    builder.compile("signal");
 }
